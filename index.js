ae204ded43f88706b2725da19ad4cbea81c252e2
Jose Alvaro Garcia <jag040393@gmail.com>
2017-04-21 22:03:47 -0700

    Added everything, this is probably the worst idea for a commit but oh well

---
+++ index.js
@@ -0,0 +1,47 @@
+const nodemailer = require('nodemailer');
+
+//Username and Password for email that will be sent out to people who have information
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'jg93439343@gmail.com',
+        pass: 'test12345678'
+    }
+});
+
+//Email List with contact details
+let mailList = {
+	first: {email:'jag040393@gmail.com',
+         	name: 'Jose Garcia' 
+		   },
+	second: {email:'joseabgweb@gmail.com',
+			 name: 'Purple Joe'
+			}
+}
+
+//Function that will be ran once per contact in list 
+var toMail = (mailList)=>{ 
+
+Object.keys(mailList).map((keyOfMailList)=>{
+	let mailOptions = {
+    	from: '"Jose Garcia" <jg93439343@gmail.com>', 
+    	to: mailList[keyOfMailList].email, 
+    	subject: `Hello ${mailList[keyOfMailList].name}`, 
+    	text: `Hello ${mailList[keyOfMailList].name} please give me the information I need`, 
+	};
+
+
+		transporter.sendMail(mailOptions, (error, info) => {
+    		if (error) {
+        		return console.log(error);
+    		}
+    		console.log('Message %s sent: %s', info.messageId, info.response);
+			});
+	})
+
+}
+
+
+// I will call the function in the end of this code
+toMail(mailList);
+
